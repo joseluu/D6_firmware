e56af2ead9203175271aa64d1548a67ab81177b1
joseluu <jose.luu@gmail.com>
2019-03-22 19:57:03 +0100

    blink both leds for better visual effect

--- tests/D6_blinkTest/LEDBlink.cpp
+++ tests/D6_blinkTest/LEDBlink.cpp
@@ -15,6 +15,7 @@ int main(void)
 	HAL_Init();
 
 	__GPIOA_CLK_ENABLE();
+	__GPIOB_CLK_ENABLE();
 	GPIO_InitTypeDef GPIO_InitStructure;
 
 	GPIO_InitStructure.Pin = GPIO_PIN_11;
@@ -23,12 +24,16 @@ int main(void)
 	GPIO_InitStructure.Speed = GPIO_SPEED_HIGH;
 	GPIO_InitStructure.Pull = GPIO_NOPULL;
 	HAL_GPIO_Init(GPIOA, &GPIO_InitStructure);
+	GPIO_InitStructure.Pin = GPIO_PIN_1;
+	HAL_GPIO_Init(GPIOB, &GPIO_InitStructure);
 
 	for (;;)
 	{
 		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
 		HAL_Delay(500);
 		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
 		HAL_Delay(500);
 	}
 }
